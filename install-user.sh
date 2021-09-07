0cc28a729aff8ebbc7f7bca1578cec8996192bba
Ian Ardiansyah <ian.ian.wold76@gmail.com>
2021-09-08 01:58:06 +0700

    edit install user

--- install-user.sh
+++ install-user.sh
@@ -6,6 +6,25 @@ echo ""
 echo "Input yout Username here"
 read username
 
+if [[ ! $username =~ ^[a-z][-a-z0-9_]*\$ ]] then
+  echo "Continue using bad username? (Press \"y\" for continue)"
+read confirm
+if [[ ! $confirm =~ ^(y|Y)$ ]] then
+echo "Fail by bad name!"
+exit
+fi
+fi
+
+echo "add your password:"
+read -s password
+echo "confirm password:"
+read -s confirm
+
+if [ $password != $confirm ] then
+echo "Fail by password not couple"
+exit
+fi
+
 echo ""
 echo "check apt update"
 apt update -y
@@ -14,7 +33,7 @@ echo "installing sudo"
 apt install sudo
 echo ""
 echo "create $username"
-adduser $username
+adduser --password $password $username
 echo ""
 echo "add $username to sudo group"
 usermod -aG sudo $username
@@ -27,6 +46,47 @@ echo ""
 echo "create hush login"
 touch /home/$username/.hushlogin
 echo ""
+echo "creating new start linux"
+echo ""
+
+bin=ustart.sh
+echo "writing launch script"
+cat > $bin <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A ubuntu-binds)" ]; then
+    for f in ubuntu-binds/* ;do
+      . \$f
+    done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b ubuntu-fs/root:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+#command+=" -b /sdcard"
+command+=" -w /home/$username"
+command+=" /usr/bin/env -i"
+command+=" HOME=/home/$username"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" bin/su - billy1 -s /bin/bash"
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+
 echo ""
 clear
 echo "completely!!!"
@@ -35,14 +95,6 @@ echo ""
 
 echo "Whats next??"
 echo "- press \"exit\" here!"
-echo "- backup \"./start-ubuntu.sh\""
-echo "cp ./start-linux.sh ./start-linux.old"
-echo "- edit using nano"
-echo "nano ./start-linux.sh"
-echo "- follow what i edit...:"
+echo "- run this command"
 echo ""
-echo "command+=\" -w /root\"  ->  command+=\" -w /home/$username\""
-echo "command+=\" HOME=/root\"  ->. command+=\" HOME=/home/$username\""
-echo "command+=\" /bin/bash --login\"  ->  command+=\"bin/su - billy1 -s /bin/bash\""
-echo ""
-echo "- Save (Ctrl + S) and exit (Ctrl + X)"
+echo "wget https://raw.githubusercontent.com/dinastishitpost/script/main/post.sh && ./post.sh && rm ./post.sh"
